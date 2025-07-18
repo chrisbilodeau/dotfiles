64f3b52313a9d7e2eba029a8c5d2328f0d47d25d
Chris Bilodeau <7334990+chrisbilodeau@users.noreply.github.com>
2025-07-17 19:16:56 -0600

    fix: create bat themes dir if needed

--- home/.chezmoiscripts/run_once_after-bat-theme.sh
+++ home/.chezmoiscripts/run_once_after-bat-theme.sh
@@ -4,6 +4,7 @@ set -eufo pipefail
 
 echo "Downloading bat Catppuccin Latte theme..."
 THEMES_DIR="$(bat --config-dir)/themes"
+mkdir -p $THEMES_DIR
 curl --silent --fail --show-error --output-dir $THEMES_DIR --output Catppuccin-Latte.tmTheme  --location "https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme"
 echo "Rebuilding bat cache..."
 bat cache --build
